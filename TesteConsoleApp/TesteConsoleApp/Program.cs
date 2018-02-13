5b55fa28c4841c30d9843af2c1d727c0b84a5294
lerder22 <alexlrdr@hotmail.com>
2018-02-12 22:25:36 -0300

    Added new message.

--- TesteConsoleApp/TesteConsoleApp/Program.cs
+++ TesteConsoleApp/TesteConsoleApp/Program.cs
@@ -11,6 +11,7 @@ namespace TesteConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Exemplo de uso do GitHub  com o Visual Studio 2017");
+            Console.WriteLine("Hello world!!");
             Console.ReadKey();
         }
     }
